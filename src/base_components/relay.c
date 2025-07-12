43777c960209126532d0bacf3befc14975f089ec
aceindy <aceindy@gmail.com>
2025-07-12 19:25:46 +0200

    Update relay.c
    clear pulse after 200ms

--- src/base_components/relay.c
+++ src/base_components/relay.c
@@ -13,17 +13,15 @@ void relay_on(relay_t *relay)
   printf("relay_on\r\n");
 
   if (relay->off_pin) {
-    // Bi-stable Latch relay: Pulse ON pin for 125ms
-    drv_gpio_write(relay->pin, relay->on_high);
-    WaitMs(125);
-    drv_gpio_write(relay->pin, !relay->on_high);
+    // Bistable relay: non-blocking pulse
+    relay_pulse_pin(relay->pin, relay->on_high);
   } else {
-    // Normal relay: Turn on the active pin
+    // Normal relay: stay powered
     drv_gpio_write(relay->pin, relay->on_high);
   }
+
   relay->on = 1;
-  if (relay->on_change != NULL)
-  {
+  if (relay->on_change) {
     relay->on_change(relay->callback_param, 1);
   }
 }
@@ -33,17 +31,15 @@ void relay_off(relay_t *relay)
   printf("relay_off\r\n");
 
   if (relay->off_pin) {
-    // Bi-stable Latch relay: Pulse OFF pin for 125ms
-    drv_gpio_write(relay->off_pin, relay->on_high);
-    WaitMs(125);
-    drv_gpio_write(relay->off_pin, !relay->on_high);
+    // Bistable relay: non-blocking pulse
+    relay_pulse_pin(relay->off_pin, relay->on_high);
   } else {
-    // Normal relay: Turn off the active pin
+    // Normal relay: turn off power
     drv_gpio_write(relay->pin, !relay->on_high);
   }
+
   relay->on = 0;
-  if (relay->on_change != NULL)
-  {
+  if (relay->on_change) {
     relay->on_change(relay->callback_param, 0);
   }
 }
